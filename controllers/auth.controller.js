1deae65b1e4a91e0e7fedb302a6d2948669345e5
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2026-05-05 14:00:30 +0100

    auth and user controller taken care of

---
+++ controllers/auth.controller.js
@@ -0,0 +1,78 @@
+const authRoute = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+
+const { validateSignup, validateLogin } = require("../validators/validate");
+const validate = require("../middleware/validator.middleware");
+const User = require("../models/user.model");
+
+
+const signup = async function (req, res) {
+
+     try {
+        const { firstName, lastName, username, email, password } = req.body;
+    
+        const userExist = await User.findOne({ email });
+    
+        if (userExist) {
+          return res.status(400).json({ error: "User already exists" });
+        }
+    
+        const usernameTaken = await User.findOne({ username });
+    
+        if (usernameTaken) {
+          return res.status(400).json({ error: "Username already taken" });
+        }
+    
+        const newUser = await User.create({
+          firstName,
+          lastName,
+          username,
+          email,
+          password,
+        });
+    
+        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        
+        res.redirect("/posts")
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+}
+
+const login = async (req, res, next) => {
+
+     passport.authenticate("login", { session: false }, (err, user, info) => {
+        try {
+          if (err) {
+            return res.status(500).json({ error: err.message });
+          }
+    
+          if (!user) {
+            return res.status(400).json({ error: info.message });
+          }
+    
+          req.login(user, { session: false }, (err) => {
+            if (err) {
+              return res.status(500).json({ error: err.message });
+            }
+    
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+              expiresIn: "1h",
+            });
+    
+            res.render("posts", { token });
+          });
+        } catch (err) {
+          return res.status(500).json({ error: err.message });
+        }
+      })(req, res, next);
+}
+
+module.exports = {
+    signup,
+    login
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,8 @@
 const authRoute = require("express").Router();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const passport = require("passport");
 
+const authController = require("../controllers/auth.controller");
 const { validateSignup, validateLogin } = require("../validators/validate");
 const validate = require("../middleware/validator.middleware");
-const User = require("../models/user.model");
 
 authRoute.get("/", (req, res) => {
   res.render("index");
@@ -19,68 +16,8 @@ authRoute.get("/login", (req, res) => {
   res.render("login");
 });
 
-authRoute.post("/signup", validateSignup, validate, async (req, res) => {
-  console.log("BODY:", req.body); // 👈 see what's coming in
-  console.log("ERRORS:", validationResult(req).array());
-  try {
-    const { firstName, lastName, username, email, password } = req.body;
+authRoute.post("/signup", validateSignup, validate, authController.signup);
 
-    const userExist = await User.findOne({ email });
-
-    if (userExist) {
-      return res.status(400).json({ error: "User already exists" });
-    }
-
-    const usernameTaken = await User.findOne({ username });
-
-    if (usernameTaken) {
-      return res.status(400).json({ error: "Username already taken" });
-    }
-
-    const newUser = await User.create({
-      firstName,
-      lastName,
-      username,
-      email,
-      password,
-    });
-
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    
-    res.redirect("/posts")
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-authRoute.post("/login", validateLogin, validate, async (req, res, next) => {
-  passport.authenticate("login", { session: false }, (err, user, info) => {
-    try {
-      if (err) {
-        return res.status(500).json({ error: err.message });
-      }
-
-      if (!user) {
-        return res.status(400).json({ error: info.message });
-      }
-
-      req.login(user, { session: false }, (err) => {
-        if (err) {
-          return res.status(500).json({ error: err.message });
-        }
-
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-          expiresIn: "1h",
-        });
-
-        res.render("posts", { token });
-      });
-    } catch (err) {
-      return res.status(500).json({ error: err.message });
-    }
-  })(req, res, next);
-});
+authRoute.post("/login", validateLogin, validate, authController.login);
 
 module.exports = authRoute;
